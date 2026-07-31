40a75d7f48997c61b2ae63f5dd191dc44a9408fa
ducthanhnguyen04 <ndt.ducthanh04@gmail.com>
2026-08-01 05:03:37 +0800

    update be

--- config/config.js
+++ config/config.js
@@ -16,10 +16,13 @@ module.exports = {
     dialect: process.env.DB_DRIVER,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DRIVER,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Quan trọng: Supabase yêu cầu cấu hình SSL này
+      }
+    }
   },
 };
